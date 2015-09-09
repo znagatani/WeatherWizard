b5d186a2a8b61ca1caa1cb4b3da683426f4975e6
znagatani <znagatani@gmail.com>
2015-09-09 15:43:33 -0700

    add get-contents

--- scraper.php
+++ scraper.php
@@ -1,10 +1,6 @@
 <?php
 
 echo
-$contents=file_get_contents("http://www.weather-forecast.com/locations/San-Francisco/forecasts/latest");
-
-preg_match("/3 Day Weather Forecast Summary:/i", $contents, $matches);
-
-print_r($matches);
+file_get_contents("http://www.weather-forecast.com/locations/San-Diego/forecasts/latest");
 
 ?>
